e0f0b7867549cd6e40f05c6b25b80bae71b8db3b
Maksim Berezin <mms2000@mail.ru>
2024-08-19 10:55:22 +0300

    Add workflow Final with Telegram

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.87.202', 'mms2000.ddns.net']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mms2000.ddns.net']
 
 
 # Application definition
